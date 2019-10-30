8cb541088461c31b7bd595dcd9450234a092d69f
Matthieu <parismatthieu@yahoo.fr>
2019-10-30 15:32:58 +0100

    add middleware

--- src/components/containers/Store.js
+++ src/components/containers/Store.js
@@ -2,7 +2,11 @@ import {createStore, applyMiddleware} from 'redux'
 import todoApp from '../reducers/reducers'
 import thunk from 'redux-thunk';
 
+const customMiddleWare = store => next => action => {
+    console.log("Middleware triggered:", action);
+    next(action);
+};
 
-const store = createStore(todoApp, applyMiddleware(thunk));
+const store = createStore(todoApp, applyMiddleware(thunk, customMiddleWare));
 
 export default store;
